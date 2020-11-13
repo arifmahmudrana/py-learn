bdf41e3548d2335b9c101f493171d9f984e69643
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2020-11-13 22:34:35 +0600

    types print python

---
+++ types.py
@@ -0,0 +1,16 @@
+class BigObject():
+    pass
+
+
+big_obj = BigObject()
+
+print(type(None))
+print(type(True))
+print(type(5))
+print(type(5.5))
+print(type('hi'))
+print(type([]))
+print(type({}))
+print(type(()))
+print(BigObject)
+print(big_obj)
