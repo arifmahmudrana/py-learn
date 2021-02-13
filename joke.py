c0fbaae912ccc9f97f679f8a0fc8a9e68e98adc1
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2021-02-14 00:39:53 +0600

    allow jokes term search

--- joke.py
+++ joke.py
@@ -1,6 +1,10 @@
 import requests
 
-URL = 'https://icanhazdadjoke.com/'
+URL = 'https://icanhazdadjoke.com/search'
 
-with requests.get(URL, headers={"accept": "application/json"}) as res:
-    print(res.json()['joke'] + ' 😂️🤣️😎️😁️')
+term = input('Term of your joke? ')
+
+with requests.get(URL, headers={"accept": "application/json"}, params={'term': term, 'limit': 1}) as res:
+    data = res.json()
+    print(f'Jokes found with term \'{data["search_term"]}\' total found {data["total_jokes"]}', (
+        '\n' + data['results'][0]['joke'] + ' 😂️🤣️😎️😁️') if len(data['results']) else "")
