30de6ab66d1d9165098d879fd4f6a30352dbbe40
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2021-02-14 01:25:26 +0600

    dad joke

--- joke.py
+++ joke.py
@@ -1,10 +1,50 @@
+import random
 import requests
+from pyfiglet import Figlet
+from termcolor import cprint, COLORS
 
-URL = 'https://icanhazdadjoke.com/search'
 
-term = input('Term of your joke? ')
+class Joke:
+    URL = 'https://icanhazdadjoke.com'
 
-with requests.get(URL, headers={"accept": "application/json"}, params={'term': term, 'limit': 1}) as res:
-    data = res.json()
-    print(f'Jokes found with term \'{data["search_term"]}\' total found {data["total_jokes"]}', (
-        '\n' + data['results'][0]['joke'] + ' 😂️🤣️😎️😁️') if len(data['results']) else "")
+    def __init__(self):
+        self.topic = None
+        self.total_jokes = None
+        self.joke = None
+        self.print_intro()
+        self.ask_topic()
+        self.save_joke_data(self.search_joke())
+        self.print(self.get_text())
+
+    def print_intro(self):
+        cprint(Figlet().renderText('Dad Joke 3000'),
+               random.choice(tuple(COLORS.keys())), attrs=['blink'])
+
+    def ask_topic(self):
+        self.topic = input('Let me tell you a joke! Give me a topic: ')
+
+    def search_joke(self):
+        return requests.get(
+            self.URL + '/search', headers={"accept": "application/json"}, params={'term': self.topic, 'limit': 30}).json()
+
+    def save_joke_data(self, data):
+        self.topic = data['search_term']
+        self.total_jokes = data['total_jokes']
+        if self.total_jokes:
+            self.joke = random.choice(data['results'])['joke']
+
+    def get_text(self):
+        if not self.total_jokes:
+            return f"Sorry, I don't have any jokes about {self.topic}! Please try again"
+
+        if self.total_jokes is 1:
+            return f"I've got one joke about {self.topic}. Here it is: \n{self.joke}"
+
+        return f"I've got {self.total_jokes} jokes about {self.topic}. Here's one: \n{self.joke}"
+
+    def print(self, text):
+        print(text)
+
+
+if __name__ == '__main__':
+    Joke()
