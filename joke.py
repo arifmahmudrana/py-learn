d7fa37613fdc5858a1349ba8311b4e3217eabf95
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2021-03-03 02:01:25 +0600

    refactor make private and format code

--- joke.py
+++ joke.py
@@ -1,16 +1,17 @@
 import random
+
 import requests
 from pyfiglet import Figlet
-from termcolor import cprint, COLORS
+from termcolor import COLORS, cprint
 
 
 class Joke:
     URL = 'https://icanhazdadjoke.com'
 
     def __init__(self):
-        self.topic = None
-        self.total_jokes = None
-        self.joke = None
+        self._topic = None
+        self._total_jokes = None
+        self._joke = None
         self.print_intro()
         self.ask_topic()
         self.save_joke_data(self.search_joke())
@@ -22,23 +23,23 @@ class Joke:
                random.choice(tuple(COLORS.keys())), attrs=['blink'])
 
     def ask_topic(self):
-        self.topic = input('Let me tell you a joke! Give me a topic: ')
+        self._topic = input('Let me tell you a joke! Give me a topic: ')
 
     def search_joke(self):
         return requests.get(
-            self.URL + '/search', headers={"accept": "application/json"}, params={'term': self.topic, 'limit': 30}).json()
+            self.URL + '/search', headers={"accept": "application/json"}, params={'term': self._topic, 'limit': 30}).json()
 
     def save_joke_data(self, data):
-        self.topic = data['search_term']
-        self.total_jokes = data['total_jokes']
-        if self.total_jokes:
-            self.joke = random.choice(data['results'])['joke']
+        self._topic = data['search_term']
+        self._total_jokes = data['total_jokes']
+        if self._total_jokes:
+            self._joke = random.choice(data['results'])['joke']
 
     def get_text(self):
-        if not self.total_jokes:
+        if not self._total_jokes:
             return f"Sorry, I don't have any jokes about {self.topic}! Please try again"
 
-        if self.total_jokes is 1:
+        if self._total_jokes is 1:
             return f"I've got one joke about {self.topic}. Here it is: \n{self.joke}"
 
         return f"I've got {self.total_jokes} jokes about {self.topic}. Here's one: \n{self.joke}"
