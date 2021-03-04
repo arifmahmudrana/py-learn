ca05327597b990b724b03bcd39993557ca9d50fe
Arif Mahmud Rana <arif_mahmud_rana@hotmail.com>
2021-03-05 01:47:13 +0600

    rewrite random password generator

--- rand.py
+++
@@ -1,9 +0,0 @@
-import string
-import random
-
-
-chars = [random.choice(string.ascii_uppercase) for _ in range(
-    3)] + [random.choice(string.ascii_lowercase) for _ in range(3)] + [random.choice(string.digits) for _ in range(3)] + [random.choice(string.punctuation) for _ in range(3)]
-
-random.shuffle(chars)
-print(''.join(chars))
---
+++ random_password_generator.py
@@ -0,0 +1,26 @@
+
+from random import choice, randint, shuffle
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
+from sys import argv
+
+pass_length = int(argv[-1]) if len(argv) > 1 else 12
+
+charset = [ascii_uppercase, ascii_lowercase, punctuation, digits]
+
+shuffle(charset)
+
+first = [choice(charset[0]) for _ in range(randint(1, 6))]
+
+second = [choice(charset[1])
+          for _ in range(randint(1, pass_length - len(first) - 2))]
+
+third = [choice(charset[2]) for _ in range(
+    randint(1, pass_length - len(first) - len(second) - 1))]
+
+fourth = [choice(charset[3]) for _ in range(
+    randint(1, pass_length - len(first) - len(second) - len(third)))]
+
+chars = first + second + third + fourth
+
+shuffle(chars)
+print(''.join(chars), end='')
